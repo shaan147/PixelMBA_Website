c2bab0c73889aec2d372a82f676101c22dc2663a
akramZafar1 <akram@quantomex.com>
2023-11-29 20:27:59 +0500

    checkDiv

--- routes/homePageRoute.js
+++ routes/homePageRoute.js
@@ -8,4 +8,7 @@ router.get('/', async (req, res) => {
     res.render('./user_pages/home');
 });
 
+router.get('/grid', async (req, res) => {
+    res.render('./user_pages/grid');
+  });
 module.exports = router;
