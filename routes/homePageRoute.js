2b9c601283c10bcabbc20a0e8dd016b06bac3e85
Shaan E Mustafa <70950454+shaan147@users.noreply.github.com>
2023-12-01 16:58:20 +0500

    some more changes~!~

--- routes/homePageRoute.js
+++ routes/homePageRoute.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
+const user = require('../models/user');
 
 router.get('/', async (req, res) => {  
-
     res.render('./user_pages/home');
 });
 
 router.get('/grid', async (req, res) => {
-    res.render('./user_pages/grid');
+  res.render('./user_pages/grid', { user: req.user }); 
   });
 
   router.get('/signup', async (req, res) => {
